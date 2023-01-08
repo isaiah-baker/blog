4c8a9301e79f1beb8e42bcb9055cc8d5d0eb8f80
Isaiah Baker <ibkr10@gmail.com>
2023-01-08 04:37:50 -0600

    commit

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,10 +4,10 @@ export const getGlobalData = () => {
     : 'Isaiah Baker';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'isaiah@isaiahbaker.xyz';
+    : 'Tech adventures';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'isaiah@isaiahbaker.xyz';
 
   return {
     name,
